a2250014ddc2c043d254de95692686738f7a4682
Rasik Prajapati <56621182+arr-swartz@users.noreply.github.com>
2021-06-05 09:13:56 +0530

    django webapp

---
+++ Django_Webapp/driver_drowsiness/driver_drowsiness/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for driver_drowsiness project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'driver_drowsiness.settings')
+
+application = get_asgi_application()
